3a9cfa78ceebce038f821fae194797db6af1aaba
Sirano98 <protsiuk1998@gmail.com>
2021-03-25 21:49:10 +0200

    feat: add solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (arr instanceof Array) {
+    const controlSequences = ["--discard-next", "--discard-prev", "--double-next", "--double-prev"];
+    let transformedArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      switch (arr[i]) {
+
+        case "--discard-prev":
+          if (i !== 0) {
+            transformedArray.pop();
+          };
+          break;
+
+        case "--discard-next":
+          if (i !== arr.length - 1 && arr[i + 2] !== "--discard-prev") {
+            i++;
+          };
+          break;
+
+        case "--double-next":
+          if (i !== arr.length - 1) {
+            transformedArray.push(arr[i + 1], arr[i + 1]);
+            i += 1;
+          };
+          break;
+
+        case "--double-prev":
+          if (i !== 0 && arr[i - 2] !== "--discard-next") {
+            transformedArray.push(arr[i - 1]);
+          };
+          break;
+
+        default:
+          transformedArray.push(arr[i]);
+      };
+    };
+    return transformedArray;
+  } else {
+    throw new Error();
+  };
 };
