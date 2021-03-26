5b4facc0cc17ec47962f087ae194e41e3f22a0fa
Sirano98 <protsiuk1998@gmail.com>
2021-03-27 00:06:27 +0200

    feat: add solution for vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,80 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-}
+  encrypt(message, key) {
+
+    if (message && key) {
+
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+
+      let result = "",
+        i,
+        k = 0,
+        size = message.length;
+
+      for (i = 0; i < size; i++) {
+
+        if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+
+          if (k >= key.length) {
+            k = 0;
+          };
+          result += String.fromCharCode(((message.charCodeAt(i) + key.charCodeAt(k)) % 26) + 65);
+          k++;
+        } else {
+          result += message[i];
+        };
+
+      };
+
+      if (this.isDirect) {
+        return result;
+      };
+      return result.split("").reverse().join("");
+
+    } else {
+      throw new Error();
+    };
+  }
+  decrypt(message, key) {
+    if (message && key) {
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+
+      let result = "",
+        i,
+        k = 0,
+        size = message.length;
+
+      for (i = 0; i < size; i++) {
+
+        if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+
+          if (k >= key.length) {
+            k = 0;
+          };
+          result += String.fromCharCode(((message.charCodeAt(i) + 26 - key.charCodeAt(k)) % 26) + 65);
+          k++;
+        } else {
+          result += message[i];
+        };
+
+      };
+
+      if (this.isDirect) {
+        return result;
+      };
+
+      return result.split("").reverse().join("");
+
+    } else {
+      throw new Error();
+    };
+  };
+};
 
 module.exports = VigenereCipheringMachine;
